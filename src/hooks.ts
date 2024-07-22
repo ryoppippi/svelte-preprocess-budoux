f48bdec158f762040f06ac37ca334f5f95e8bfb6
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-22 11:44:54 +0100

    feature: enable to change languages by attribute

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,13 +1,11 @@
 import type { Handle } from "@sveltejs/kit";
 import { parse } from "node-html-parser";
 import { Options } from "./types";
-import { getParser, resolveOptions } from "./utils";
+import { getParser, Language, resolveOptions } from "./utils";
 import { MagicStringAST } from "magic-string-ast";
 
 export function budouxHandle(options: Options = {}): Handle {
   const { language, attribute } = resolveOptions(options);
-
-  const parser = getParser(language);
 
   return async ({ event, resolve }) => {
     return await resolve(event, {
@@ -16,6 +14,9 @@ export function budouxHandle(options: Options = {}): Handle {
         const parsedHTML = parse(html);
         const elements = parsedHTML.querySelectorAll(`[${attribute}]`);
         for (const element of elements) {
+          const attr = element.attributes[attribute]
+          const parser = getParser(attr === '' ? language : attr as Language);
+
           const { range, innerHTML } = element;
           const [start, end] = range;
           const parsed = parser.translateHTMLString(innerHTML);
