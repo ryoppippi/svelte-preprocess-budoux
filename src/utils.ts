8ffaedc227dce3e5feb9cd551ba1fc3efe691f8d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-04 15:38:11 +0100

    format

--- src/utils.ts
+++ src/utils.ts
@@ -1,31 +1,33 @@
-import defu from "defu";
-import * as budoux from "budoux";
-import { DEFAULT_OPTIONS, Options } from "./types";
+import defu from 'defu';
+import * as budoux from 'budoux';
+import type { Options } from './types';
+import { DEFAULT_OPTIONS } from './types';
 
 export type HTMLProcessingParser = budoux.HTMLProcessingParser;
-export type Language = Exclude<Options["language"], null | undefined>;
+export type Language = Exclude<Options['language'], null | undefined>;
 
 export function resolveOptions(options: Options) {
-  return defu(options, DEFAULT_OPTIONS);
+	return defu(options, DEFAULT_OPTIONS);
 }
 
 /** Cache for the parser */
-let parserCache: Record<string, HTMLProcessingParser> = {};
+const parserCache: Record<string, HTMLProcessingParser> = {};
 
 export function getParser(
-  language: Language,
+	language: Language,
 ): HTMLProcessingParser {
-  switch (language) {
-    case "ja":
-      return parserCache["ja"] ??= budoux.loadDefaultJapaneseParser();
-    case "cs":
-      return parserCache["cs"] ??= budoux.loadDefaultSimplifiedChineseParser();
-    case "ct":
-      return parserCache["ct"] ??= budoux.loadDefaultTraditionalChineseParser();
-    case "th":
-      return parserCache["th"] ??= budoux.loadDefaultThaiParser();
-    default:
+	switch (language) {
+		case 'ja':
+			return parserCache.ja ??= budoux.loadDefaultJapaneseParser();
+		case 'cs':
+			return parserCache.cs ??= budoux.loadDefaultSimplifiedChineseParser();
+		case 'ct':
+			return parserCache.ct ??= budoux.loadDefaultTraditionalChineseParser();
+		case 'th':
+			return parserCache.th ??= budoux.loadDefaultThaiParser();
+		default:
       language satisfies never;
-      throw new Error(`Language ${language} is not supported`);
-  }
+			// eslint-disable-next-line ts/restrict-template-expressions
+			throw new Error(`Language ${language} is not supported`);
+	}
 }
