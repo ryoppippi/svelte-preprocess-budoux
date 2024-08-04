8de6f20556ddd3651e6eb4c83c9f795492dfa8e4
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-04 15:38:11 +0100

    format

--- src/types.ts
+++ src/types.ts
@@ -1,26 +1,25 @@
-import type { BaseNode } from "estree";
+import type { BaseNode } from 'estree';
 
-export interface Node extends BaseNode {
-  name: string;
-  start: number;
-  end: number;
-  attributes: Array<{ name: string; type: string }>;
-  children?: Array<Node>;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  data?: unknown;
-}
+export type Node = {
+	name: string;
+	start: number;
+	end: number;
+	attributes: Array<{ name: string; type: string }>;
+	children?: Array<Node>;
+	data?: unknown;
+} & BaseNode;
 
 /**
  * Options for the budoux preprocessor
  * @param language - The default language to translate to (default: `ja`)
  * @param attribute - The attribute to look for to translate (default: `data-budoux`)
  */
-export interface Options {
-  language?: "ja" | "cs" | "ct" | "th";
-  attribute?: string;
-}
+export type Options = {
+	language?: 'ja' | 'cs' | 'ct' | 'th';
+	attribute?: string;
+};
 
 export const DEFAULT_OPTIONS = {
-  language: "ja",
-  attribute: `data-budoux`,
+	language: 'ja',
+	attribute: `data-budoux`,
 } as const satisfies Options;
