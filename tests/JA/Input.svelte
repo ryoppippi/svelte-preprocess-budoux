da3854159518dab4c82bc3b4d77698a3dfba58d3
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-05 19:52:06 +0100

    feat(test): added tests for nested tag

--- tests/JA/Input.svelte
+++ tests/JA/Input.svelte
@@ -1,1 +1,14 @@
 <p data-budoux> 本日は晴天です。明日は曇りでしょう。 </p>
+
+<div>
+  <p data-budoux> 今日は晴れです。明日は雨でしょう。 </p>
+</div>
+
+<header>
+  <p> this is the normal text </p>
+  <div>
+    <p data-budoux> 今日は晴れです。明日は雨でしょう。 </p>
+  </div>
+</header>
+
+<p data-budoux> 今日は晴れです。明日は雨でしょう。 </p>
--- tests/JA/Output.svelte
+++ tests/JA/Output.svelte
@@ -1,1 +1,14 @@
 <p data-budoux="" style="word-break: keep-all; overflow-wrap: anywhere;"> 本日は​晴天です。​明日は​曇りでしょう。​ </p>
+
+<div>
+  <p data-budoux="" style="word-break: keep-all; overflow-wrap: anywhere;"> ​今日は​晴れです。​明日は​雨でしょう。​ </p>
+</div>
+
+<header>
+  <p> this is the normal text </p>
+  <div>
+    <p data-budoux="" style="word-break: keep-all; overflow-wrap: anywhere;"> ​今日は​晴れです。​明日は​雨でしょう。​ </p>
+  </div>
+</header>
+
+<p data-budoux="" style="word-break: keep-all; overflow-wrap: anywhere;"> ​今日は​晴れです。​明日は​雨でしょう。​ </p>
